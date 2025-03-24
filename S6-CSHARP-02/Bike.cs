73c5ee32e76d96abf60a75321e61b0177037ea2b
Justin Bottinga <0337445@student.rocvantwente.nl>
2025-03-24 14:19:40 +0100

    opdracht 1

---
+++ S6-CSHARP-02/Bike.cs
@@ -0,0 +1,38 @@
+﻿using S6_CSHARP_02.Interfaces;
+
+namespace S6_CSHARP_02
+{
+    public class Bike : IVehicle
+    {
+
+        public int Speed { get; set; }
+        public int Wheels { get; set; }
+        public string Color { get; set; }
+
+        public Bike(int speed, int wheels, string color)
+        {
+            Speed = speed;
+            Wheels = wheels;
+            Color = color;
+        }
+
+    public string SpeedUp()
+        {
+            Speed += 10;
+
+            return "The bike is speeding up!";
+        }
+
+        public string SlowDown()
+        {
+            Speed -= 10;
+
+            return "The bike is slowing down!";
+        }
+
+        public string Print()
+        {
+            return "The bike is going at " + Speed + " mph. It has " + Wheels + " wheels and is " + Color + " in color.";
+        }
+    }
+}
---
+++ S6-CSHARP-02/Interfaces/IVehicle.cs
@@ -0,0 +1,16 @@
+﻿
+namespace S6_CSHARP_02.Interfaces
+{
+    public interface IVehicle
+    {
+        public int Speed { get; set; }
+        public int Wheels { get; set; }
+        public string Color { get; set; }
+
+        public string SpeedUp();
+
+        public string SlowDown();
+
+        public string Print();
+    }
+}
